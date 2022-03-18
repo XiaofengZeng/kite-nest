5ecc93c483116b5f6fa4c89ed5c41b3259416294
XiaofengZeng <798065964@qq.com>
2022-03-18 09:12:21 +0800

    feat(module): 完成用户卡片UserCard中的按钮操作（运维+退出）
    1. 使用Element UI组件优化用户卡片按钮（前往运维系统+退出用户）并完成操作逻辑
    2. 暂未实现运维系统，因此暂做提示处理

--- src/components/SideMenu/UserCard/index.vue
+++ src/components/SideMenu/UserCard/index.vue
@@ -5,13 +5,13 @@
     </div>
     <div class="user-info">
       <span>用户名: {{userName}}</span>
-      <br>
       <!-- TODO：接入后端 -->
+      <!-- <br> -->
       <!-- <span>角色: </span> -->
     </div>
     <div class="user-options">
-      <span class="el-icon-setting"></span>
-      <span class="el-icon-switch-button"></span>
+      <el-button type="text" @click="goToManage" class="el-icon-setting"></el-button>
+      <el-button type="text" @click="logout" class="el-icon-switch-button"></el-button>
     </div>
   </div>
 </template>
@@ -27,6 +27,30 @@ export default {
   methods: {
     getUserName() {
       return JSON.parse(localStorage.getItem('kn-token')).name;
+    },
+    goToManage() {
+      // TODO: 用户运维系统
+      const h = this.$createElement;
+      this.$notify({
+        title: '提醒',
+        message: h('i', { style: 'color: #f5995a' }, '开发中...'),
+      });
+    },
+    logout() {
+      const name = this.getUserName();
+      this.$confirm(`当前用户 ${name} 将离开系统, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        window.localStorage.removeItem('kn-token');
+        this.$router.replace('/login');
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出操作',
+        });
+      });
     },
   },
   computed: {
@@ -58,14 +82,9 @@ export default {
   }
   .user-options {
     width: 80px;
-    font-size: 28px;
     justify-content: center;
-    span {
-      margin: 5px;
-      color: rgb(255, 255, 255);
-      :hover {
-        color: #F5995A;
-      }
+    .el-button {
+      font-size: 26px;
     }
   }
 }
