0e65fd444090955ab06a307303d8c1fbf2e2f1bb
XiaofengZeng <798065964@qq.com>
2022-03-26 14:31:12 +0800

    perf(global): 优化构建配置
    1. 优化Cesium的构建配置，将该库单独存放在cesium目录中

--- vue.config.js
+++ vue.config.js
@@ -2,40 +2,42 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
+// Cesium目录映射
 const cesiumSource = 'node_modules/cesium/Source';
 const cesiumWorkers = '../Build/Cesium/Workers';
+
 module.exports = {
   publicPath: './',
   configureWebpack: {
-    amd: {
-      toUrlUndefined: true, // webpack在cesium中能友好的使用require
-    },
     resolve: {
       extensions: ['.js', '.vue', '.json'],
+      // 定义别名，方便开发时引用文件
       alias: {
-        // 定义别名cesium后，cesium代表了cesiumSource的文件路径，此处配置好后，就在main.js中直接使用cesium引入资源
         cesium: path.resolve(__dirname, cesiumSource),
       },
     },
     plugins: [
+      // 编译时，复制目录至指定文件夹
       new CopyWebpackPlugin([
-        { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
-        { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
-        { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' },
-        { from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'ThirdParty/Workers' },
+        // Cesium的引用路径变换，配合CESIUM_BASE_URL使用
+        { from: path.join(cesiumSource, cesiumWorkers), to: 'cesium/Workers' },
+        { from: path.join(cesiumSource, 'Assets'), to: 'cesium/Assets' },
+        { from: path.join(cesiumSource, 'Widgets'), to: 'cesium/Widgets' },
+        { from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'cesium/ThirdParty/Workers' },
       ]),
 
       new webpack.DefinePlugin({
-        CESIUM_BASE_URL: JSON.stringify('./'),
+        // 修改Cesium引用路径，对应CopyWebpackPlugin中Cesium的to配置
+        CESIUM_BASE_URL: JSON.stringify('./cesium'),
       }),
     ],
     module: {
+      // 不打印webpack载入特定库时的警告
       unknownContextCritical: false,
       unknownContextRegExp: /\/cesium\/cesium\/Source\/Core\/buildModuleUrl\.js/,
     },
   },
   devServer: {
-    // 端口号
     port: 1215,
   },
 };
