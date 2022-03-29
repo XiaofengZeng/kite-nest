b9d485c32075c01bd8207d89a61f7c38607fb133
XiaofengZeng <798065964@qq.com>
2022-03-29 17:57:35 +0800

    feat(module): 推进二维地图测距功能开发进度

--- src/components/Map/Map2d/mixin/interaction/DrawingHelper/index.js
+++ src/components/Map/Map2d/mixin/interaction/DrawingHelper/index.js
@@ -1,14 +1,51 @@
-/* 绘制帮助混入 */
+/* 绘制帮助器混入 */
+
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Draw } from 'ol/interaction/Draw';
+
 export default {
   data() {
     return {
+      drawLayerSource: new VectorSource({ wrapX: false }),
     };
   },
-  methods: {
+  mounted() {
+    this.initDrawLayer();
   },
-  // 测距
-  measureDistance() {
-    // TODO: 完成测距逻辑
-    console.log(this.map2d);
+  methods: {
+    // 初始化绘制图层
+    initDrawLayer() {
+      const drawLayer = new VectorLayer({
+        source: this.drawLayerSource,
+      });
+      this.map2d.addLayer(drawLayer);
+    },
+    // 增加交互+监听绘制
+    addInteraction(type) {
+      const drawInteraction = new Draw({
+        source: this.drawLayerSource,
+        type,
+      });
+      this.map2d.addInteraction(drawInteraction);
+    },
+    // 测距
+    measureDistance() {
+      // TODO: 完成测距逻辑
+      const drawInteraction = new Draw({
+        source: this.drawLayerSource,
+        type: 'LineString',
+      });
+      function drawEndHandler(event) {
+        console.log(event);
+      }
+      drawInteraction.on('drawend', drawEndHandler());
+      this.map2d.addInteraction(drawInteraction);
+    },
+    // 测面积
+    measureArea() {
+      // TODO: 完成测面积逻辑
+      this.addInteraction('Polygon');
+    },
   },
 };
